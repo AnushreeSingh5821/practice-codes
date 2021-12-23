af2b9fd9e7cda73c9dd65d2a7692b3e315a96b00
Anushree <67423122+AnushreeSingh5821@users.noreply.github.com>
2021-12-23 18:19:21 +0530

    Add files via upload

---
+++ Palindrome Half Pyramid Patterns with Alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+   int i, j, rows, c=0;
+
+   printf("Enter rows: ");
+   scanf("%d", &rows);
+
+   for (i = 1; i <= 2*rows; i=i+2) {
+      for (j = 1; j <= i; j++) {
+       printf("%c", 'A'+c);
+       if(j <= i/2)
+           c++;
+        else
+           c--;
+      }
+      c = 0;
+      printf("\n");
+   }
+   return(0);
+}
